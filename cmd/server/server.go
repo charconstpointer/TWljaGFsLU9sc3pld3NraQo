4c38635a7f9aeb700d454a93f788487296cc1df4
charconstpointer <attwistingnether@gmail.com>
2020-07-22 23:29:22 +0200

    chi

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"flag"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	fmt.Fprintf(os.Stdout, "server\n")
+	port := flag.Int("port", 8080, "port to listen on for incoming http requests")
+	flag.Parse()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	addr := ":" + strconv.Itoa(*port)
+	http.ListenAndServe(addr, r)
 }
